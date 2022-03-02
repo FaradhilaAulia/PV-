87f0c462b375277907a6c27be0e94e78129edd95
Faradhila Aulia Utami Tanjung <85839619+FaradhilaAulia@users.noreply.github.com>
2022-03-02 15:17:54 +0700

    Add files via upload

---
+++ Materi Pertemuan 4/Materi Pertemuan 4/MainForm.cs
@@ -0,0 +1,40 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: User
+ * Date: 3/2/2022
+ * Time: 2:54 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Materi_Pertemuan_4
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		
+		void Button1Click(object sender, EventArgs e)
+		{
+			MessageBox.Show ("Perkenalkan Nama saya " + this.textBox1.Text + " ,Nim " + this.textBox2.Text + " ,Kelas " + this.textBox3.Text + " ,sedang Belajar " + this.textBox4.Text);
+		}
+		
+	}
+}
